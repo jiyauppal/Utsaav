2ec7307110c2e0f4d9a6927f1b99e099d5824479
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2021-05-16 21:14:02 +0530

    Add files via upload
    uploaded

---
+++ js.js
@@ -0,0 +1,26 @@
+function display() {
+    alert("Your Booking is Done. Confirmation will be sent on Required email.");
+
+}
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+ 
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+         
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+         
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+ 
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
+const items = document.querySelectorAll(".item");
+ 
